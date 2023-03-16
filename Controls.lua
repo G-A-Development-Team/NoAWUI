3e8ef61c044f73592c15d8966a3bf543352e7580
Jeremy <spamalotsmileface@gmail.com>
2023-03-16 16:02:29 -0700

    Controls Update

--- Controls.lua
+++ Controls.lua
@@ -1,3 +1,5 @@
+
+-- By: CarterPoe
 function CreateControl()
     return {
         Type = "control",
@@ -60,6 +62,7 @@ function CreateControl()
     }
 end
 
+-- By: CarterPoe
 function CreateAWTab(properties)
     local Control = {
         X = 0,
@@ -93,6 +96,7 @@ function CreateAWTab(properties)
     return Control
 end
 
+-- By: CarterPoe
 function CreateForm(properties)
     local Control = CreateControl()
     for _, attributeValue in ipairs(properties) do
@@ -242,7 +246,7 @@ function CreateForm(properties)
     return Control
 end
 
-
+-- By: CarterPoe
 function CreatePanel(properties)
     local Control = CreateControl()
     for _, attributeValue in ipairs(properties) do
@@ -415,6 +419,7 @@ function CreatePanel(properties)
     return Control
 end
 
+-- By: CarterPoe
 function CreateFlowLayout(properties)
     local Control = CreateControl()
     Control.ScrollHeight = 20
@@ -625,6 +630,7 @@ function CreateFlowLayout(properties)
     return Control
 end
 
+-- By: CarterPoe
 function CreateButton(properties)
     local Control = CreateControl()
     Control.Background = {50, 50, 50, 255}
@@ -776,6 +782,7 @@ function CreateButton(properties)
     return Control
 end
 
+-- By: CarterPoe
 function CreateLabel(properties)
     local Control = CreateControl()
     for _, attributeValue in ipairs(properties) do
@@ -849,6 +856,436 @@ function CreateLabel(properties)
         end
 
         return properties
+    end
+
+    return Control
+end
+
+-- By: Agentsix1
+function CreatePictureBox(properties)
+    local Control = CreateControl()
+	
+	Control.ChangeImage = function (properties, src, img)
+		
+		switch(img:lower())
+            .case("jpg", function() 
+                print(src)
+                local jpgData = http.Get(src);
+                local imgRGBA, imgWidth, imgHeight = common.DecodeJPEG(jpgData);
+                local texture = draw.CreateTexture(imgRGBA, imgWidth, imgHeight);
+                properties.BackgroundImage = texture
+            end)
+            .case("png", function() 
+				local pngData = http.Get(src);
+                local imgRGBA, imgWidth, imgHeight = common.DecodePNG(pngData);
+                local texture = draw.CreateTexture(imgRGBA, imgWidth, imgHeight);
+                properties.BackgroundImage = texture
+            end)
+            .case("svg", function() 
+				local svgData = http.Get(src);
+                local imgRGBA, imgWidth, imgHeight = common.RasterizeSVG(svgData);
+                local texture = draw.CreateTexture(imgRGBA, imgWidth, imgHeight);
+                properties.BackgroundImage = texture
+            end)
+			.default(function() print("Image Type not found. key=" .. key) end)
+            .process()
+			
+	end
+    
+	for _, attributeValue in ipairs(properties) do
+        if string.find(attributeValue, "=") then
+            local key = split(attributeValue, "=")[1]
+            local value = split(attributeValue, "=")[2]
+            switch(key:lower())
+            .case("name", function() Control.Name = value end)
+            .case("group", function() Control.Group = value end)
+            .case("parent", function() Control.Parent = value end)
+            .case("type", function() Control.Type = value end)
+            .case("x", function() Control.X = value end)
+            .case("y", function() Control.Y = value end)
+            .case("width", function() Control.Width = value end)
+            .case("height", function() Control.Height = value end)
+            .case("drag", function() Control.Drag = value end)
+            .case("mouseclick", function() Control.MouseClick = value end)
+            .case("image", function() 
+                local args = split(value, ",")
+                local type = args[1]
+                local src = args[2]
+                switch(type:lower())
+                .case("jpg", function() 
+                    print(src)
+                    local jpgData = http.Get(src);
+                    local imgRGBA, imgWidth, imgHeight = common.DecodeJPEG(jpgData);
+                    local texture = draw.CreateTexture(imgRGBA, imgWidth, imgHeight);
+                    Control.BackgroundImage = texture
+                end)
+                .case("png", function() 
+                    local pngData = http.Get(src);
+                    local imgRGBA, imgWidth, imgHeight = common.DecodePNG(pngData);
+                    local texture = draw.CreateTexture(imgRGBA, imgWidth, imgHeight);
+                    Control.BackgroundImage = texture
+                end)
+                .case("svg", function() 
+                    local svgData = http.Get(src);
+                    local imgRGBA, imgWidth, imgHeight = common.RasterizeSVG(svgData);
+                    local texture = draw.CreateTexture(imgRGBA, imgWidth, imgHeight);
+                    Control.BackgroundImage = texture
+                end)
+                .default(function() print("Image Type not found. key=" .. key) end)
+                .process() 
+            end)
+            .case("background", function() 
+                if string.find(value, "theme") then
+                    local r,g,b,a = gui.GetValue(value)
+                    Control.Background[1] = r
+                    Control.Background[2] = g
+                    Control.Background[3] = b
+                    Control.Background[4] = a
+                else
+                    local args = split(value, ",")
+                    Control.Background[1] = args[1]
+                    Control.Background[2] = args[2]
+                    Control.Background[3] = args[3]
+                    Control.Background[4] = args[4]
+                end
+            end)
+            .case("roundness", function() 
+                local args = split(value, ",")
+                Control.Roundness[1] = args[1]
+                Control.Roundness[2] = args[2]
+                Control.Roundness[3] = args[3]
+                Control.Roundness[4] = args[4]
+                Control.Roundness[5] = args[5]
+                Control.Rounded = true
+            end)
+            .default(function() print("Attribute not found. key=" .. key) end)
+            .process() 
+        end
+    end
+
+    Control.Render = function(properties, form)
+        if not properties.Visible then
+            return properties
+        end
+
+        if string.find(properties.Name, "example_paneldrag3") then
+            print(properties.X + form.X)
+            
+        end
+
+        Renderer:ShadowRectangle({properties.X + form.X, properties.Y + form.Y}, {properties.Width, properties.Height}, {0,0,0,40}, 25)
+        
+        if properties.BackgroundImage ~= nil then
+            draw.SetTexture(properties.BackgroundImage)
+		end
+        Renderer:FilledRectangle({properties.X + form.X, properties.Y + form.Y}, {properties.Width, properties.Height}, properties.Background)
+        draw.SetTexture(nil)
+
+        if properties.MouseClick ~= nil then
+            --print(control.MouseDown) 
+            if input.IsButtonReleased(1) then
+                if isMouseInRect(properties.X + form.X, properties.Y + form.Y, properties.Width, properties.Height) then
+                    --gui.Command('lua.run "' .. properties.MouseClick .. '" ')
+                end
+            end
+        end
+
+        for _, control in ipairs(properties.Children) do
+            control.Render(control, properties)
+        end
+
+        return properties
+    end
+
+    return Control
+end
+
+-- By: Agentsix1
+function CreateMusicLinkButton(properties)
+	-- Creates the control
+    local Control = CreateControl()
+	
+	Control.ForeColor = {255,255,255,255}
+	
+	-- Setting the Player icon to the control
+	Control.PlayIcon = [[
+						<svg x="0px" y="0px" viewBox="0 0 1000 1000" width="40" height="40"><title>Layer 1</title>
+						<path fill="#fff"  id="svg_1"  d="M393.9,770.4L720.7,500L393.9,229.7V770.4z M501.1,7.8C218.3,7.8,10,217.3,10,500.2c0,282.9,199.9,492,482.8,492c282.8,0,497.2-209.2,497.2-492C990,217.3,784,7.8,501.1,7.8z M501.1,929.3C264,929.3,71.9,737.2,71.9,500.1C71.9,263,264,70.9,501.1,70.9c237.1,0,429.2,192.1,429.2,429.2C930.4,737.2,738.2,929.3,501.1,929.3z"/>
+						</svg>
+					   ]]
+		-- Converting Play Icon to Texture
+		local imgRGBA, imgWidth, imgHeight = common.RasterizeSVG(Control.PlayIcon);
+        local texture = draw.CreateTexture(imgRGBA, imgWidth, imgHeight);
+	-- Setting the PlayIconTexture to the control
+	Control.PlayIconTexture = texture
+	
+    -- Loading setting from design to Control
+	for _, attributeValue in ipairs(properties) do
+        if string.find(attributeValue, "=") then
+            local key = split(attributeValue, "=")[1]
+            local value = split(attributeValue, "=")[2]
+            switch(key:lower())
+            .case("name", function() Control.Name = value end)
+            .case("group", function() Control.Group = value end)
+            .case("parent", function() Control.Parent = value end)
+            .case("type", function() Control.Type = value end)
+            .case("x", function() Control.X = value end)
+            .case("y", function() Control.Y = value end)
+            .case("width", function() Control.Width = value end)
+            .case("height", function() Control.Height = value end)
+            .case("drag", function() Control.Drag = value end)
+            .case("mouseclick", function() Control.MouseClick = value end)
+            .case("url", function() Control.URL = value end)
+            .case("text", function() 
+				value = value:gsub("_", " ")
+				Control.Text = value 
+			end)
+            .case("image", function()
+				-- Loads the image url from design and loads it to BackgroundImage
+                local args = split(value, ",")
+                local type = args[1]
+                local src = args[2]
+                switch(type:lower())
+                .case("jpg", function() 
+                    print(src)
+                    local jpgData = http.Get(src);
+                    local imgRGBA, imgWidth, imgHeight = common.DecodeJPEG(jpgData);
+                    local texture = draw.CreateTexture(imgRGBA, imgWidth, imgHeight);
+                    Control.BackgroundImage = texture
+                end)
+                .case("png", function() 
+                    local pngData = http.Get(src);
+                    local imgRGBA, imgWidth, imgHeight = common.DecodePNG(pngData);
+                    local texture = draw.CreateTexture(imgRGBA, imgWidth, imgHeight);
+                    Control.BackgroundImage = texture
+                end)
+                .case("svg", function() 
+                    local svgData = http.Get(src);
+                    local imgRGBA, imgWidth, imgHeight = common.RasterizeSVG(svgData);
+                    local texture = draw.CreateTexture(imgRGBA, imgWidth, imgHeight);
+                    Control.BackgroundImage = texture
+                end)
+                .default(function() print("Image Type not found. key=" .. key) end)
+                .process() 
+            end)
+            .case("background", function() 
+				-- Sets the Control Background color
+                if string.find(value, "theme") then
+                    local r,g,b,a = gui.GetValue(value)
+                    Control.Background[1] = r
+                    Control.Background[2] = g
+                    Control.Background[3] = b
+                    Control.Background[4] = a
+                else
+                    local args = split(value, ",")
+                    Control.Background[1] = args[1]
+                    Control.Background[2] = args[2]
+                    Control.Background[3] = args[3]
+                    Control.Background[4] = args[4]
+                end
+            end)
+            .case("roundness", function() 
+				-- This is used to define the roundness of a rect
+                local args = split(value, ",")
+                Control.Roundness[1] = args[1]
+                Control.Roundness[2] = args[2]
+                Control.Roundness[3] = args[3]
+                Control.Roundness[4] = args[4]
+                Control.Roundness[5] = args[5]
+                Control.Rounded = true
+            end)
+			-- Processes the switch bs above
+            .default(function() print("Attribute not found. key=" .. key) end)
+            .process() 
+        end
+    end
+
+	Control.Font = draw.CreateFont("Bahnschrift", 20, 100)
+	-- This is used to create the actual gui object
+    Control.Render = function(properties, form)
+		-- Checks if the object should be visual
+        if not properties.Visible then
+            return properties
+        end
+		
+		-- Drag function
+        if string.find(properties.Name, "example_paneldrag3") then
+            print(properties.X + form.X)
+            
+        end
+		
+		-- Start drawing the gui object
+		
+		draw.SetFont(Control.Font)
+		
+		-- Draw the outline of the box
+		--(cord, size, outer_color, inner_color, roundness, thickness)
+		Renderer:RoundedRectangleBorder({properties.X + form.X, properties.Y + form.Y}, {properties.Width, properties.Height}, Control.ForeColor, {0,0,0,255}, 6, 2)
+		-- Set the texture for the image
+		draw.SetTexture(Control.PlayIconTexture)
+		-- Draw the image
+		Renderer:FilledRectangle({properties.X + form.X+(properties.Width)*0.04, properties.Y + form.Y+(properties.Height)*0.12}, {properties.Width*0.24, properties.Height*0.75}, {255,255,255,255})
+		-- Remove the texture for future draws
+        draw.SetTexture(nil)
+		-- Draw the text values
+		Renderer:TextP({properties.X + form.X+(properties.Width)*0.31, properties.Y + form.Y+(properties.Height)*0.36}, Control.ForeColor, Control.Text)
+		
+		-- Is used to check if the gui object has been clicked
+        if properties.MouseClick ~= nil then
+            --print(control.MouseDown) 
+            if input.IsButtonReleased(1) then
+                if isMouseInRect(properties.X + form.X, properties.Y + form.Y, properties.Width, properties.Height) then
+                    Form:ClickedMLButton(properties.Parent, properties.Name)
+                end
+            end
+        end
+		
+		-- Renders the children of the current gui object
+        for _, control in ipairs(properties.Children) do
+            control.Render(control, properties)
+        end
+		
+		if isMouseInRect(properties.X + form.X, properties.Y + form.Y, properties.Width, properties.Height) then
+            local r,g,b,a = gui.GetValue("theme.nav.active")
+			Control.ForeColor = { r, g, b, a }
+        else
+			Control.ForeColor = {255,255,255,255}
+		end
+		
+        return properties
+    end
+
+    return Control
+end
+
+-- By: Agentsix1
+function CreateCheckbox(properties)
+	-- Creates the control
+    local Control = CreateControl()
+	
+	-- Set checkmark icon
+	Control.CheckIcon = [[		
+		<svg fill="#fff" version="1.1" id="Capa_1" width="800px" height="800px" viewBox="0 0 45.701 45.7">
+			<path d="M20.687,38.332c-2.072,2.072-5.434,2.072-7.505,0L1.554,26.704c-2.072-2.071-2.072-5.433,0-7.504
+				c2.071-2.072,5.433-2.072,7.505,0l6.928,6.927c0.523,0.522,1.372,0.522,1.896,0L36.642,7.368c2.071-2.072,5.433-2.072,7.505,0
+				c0.995,0.995,1.554,2.345,1.554,3.752c0,1.407-0.559,2.757-1.554,3.752L20.687,38.332z"/>
+		</svg>
+	]]
+	
+	-- Create checkmark texture
+	local imgRGBA, imgWidth, imgHeight = common.RasterizeSVG(Control.CheckIcon);
+    local texture = draw.CreateTexture(imgRGBA, imgWidth, imgHeight);
+	
+	-- Set Checkmark Texture
+	Control.CheckTexture = texture
+	
+	-- Set default values
+	Control.CheckState = false
+	Control.CheckColor = {255,255,255,255}
+	Control.OutlineColor = {150,150,150,255}
+	Control.BackgroundColor = {56,56,56,190}
+	Control.TextColor = {255,255,255,255}
+	Control.HoverColor = {255,0,0,255}
+	Control.TextWidth = 0
+	Control.Text = "Checkbox1"
+	
+    -- Loading setting from design to Control
+	for _, attributeValue in ipairs(properties) do
+        if string.find(attributeValue, "=") then
+            local key = split(attributeValue, "=")[1]
+            local value = split(attributeValue, "=")[2]
+            switch(key:lower())
+            .case("name", function() Control.Name = value end)
+            .case("group", function() Control.Group = value end)
+            .case("parent", function() Control.Parent = value end)
+            .case("type", function() Control.Type = value end)
+            .case("x", function() Control.X = value end)
+            .case("y", function() Control.Y = value end)
+            .case("width", function() Control.Width = value end)
+            .case("height", function() Control.Height = value end)
+            .case("drag", function() Control.Drag = value end)
+            .case("mouseclick", function() Control.MouseClick = value end)
+            .case("text", function() 
+				value = value:gsub("_", " ")
+				Control.Text = value 
+			end)
+			.case("checkstate", function() 
+				if value == "true" then
+					value = true
+				elseif value == "false" then
+					value = false
+				end
+				Control.CheckState = value
+			end)
+			-- Processes the switch bs above
+            .default(function() print("Attribute not found. key=" .. key) end)
+            .process() 
+        end
+    end
+
+	Control.Font = draw.CreateFont("Bahnschrift", 20, 100)
+	-- This is used to create the actual gui object
+    Control.Render = function(properties, form)
+		-- Checks if the object should be visual
+        if not properties.Visible then
+            return properties
+        end
+		
+		-- Drag function
+        if string.find(properties.Name, "example_paneldrag3") then
+            print(properties.X + form.X)
+            
+        end
+		
+		-- Start drawing the gui object
+		
+		-- Sets the font used by draw_TextP
+		draw.SetFont(properties.Font)
+		
+		-- Draw the outline of the box
+		Renderer:RoundedRectangleBorder({properties.X + form.X, properties.Y + form.Y}, {properties.Width, properties.Height}, properties.OutlineColor, properties.BackgroundColor, 6, 2)
+		
+		if properties.CheckState then
+			local gap = 0.15
+			draw.SetTexture(properties.CheckTexture)
+			Renderer:FilledRectangle({properties.X + form.X + (properties.Width*gap) , properties.Y + form.Y + (properties.Width*gap)},
+											{properties.Width - (properties.Width*gap*2), properties.Height - (properties.Width*gap*2)}, properties.CheckColor)
+			draw.SetTexture(nil)
+		end
+		
+		-- Renders the text values next to the checkbox
+		Renderer:TextP({properties.X + form.X+(properties.Width)*1.3, properties.Y + form.Y+(properties.Height)*0.28}, properties.TextColor, properties.Text)
+		
+		-- Sets the textwidth for mouse click and mouse hover
+		properties.TextWidth, _xx = draw.GetTextSize(properties.Text)
+		
+		-- Is used to check if the gui object has been clicked
+        if properties.MouseClick ~= nil then
+            --print(control.MouseDown) 
+            if input.IsButtonReleased(1) then
+                if isMouseInRect(properties.X + form.X, properties.Y + form.Y, properties.Width+properties.TextWidth, properties.Height) then
+					if properties.CheckState then
+						properties.CheckState = false
+					else
+						properties.CheckState = true
+					end
+                end
+            end
+        end
+		
+		-- Renders the children of the current gui object
+        for _, control in ipairs(properties.Children) do
+            control.Render(control, properties)
+        end
+		
+		if isMouseInRect(properties.X + form.X, properties.Y + form.Y, properties.Width+properties.TextWidth, properties.Height) then
+            local r,g,b,a = gui.GetValue("theme.nav.active")
+			properties.TextColor = { r, g, b, a }
+        else
+			properties.TextColor = {255,255,255,255}
+		end
+		
+		return properties
     end
 
     return Control
