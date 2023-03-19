9026b695ae42ab21f554e15d3bf01ab0a5d05c25
Carter Poe <gamechampcrafted@gmail.com>
2023-03-19 03:03:54 -0400

    5

--- inspect.code.lua
+++ inspect.code.lua
@@ -13,6 +13,11 @@ function Inspect:ShowElement(args)
         if child.Height == 200 then
             child.Height = 20
             child.Background = {1,1,1,1}
+            for k, v in pairs(child.Children) do
+                if v.Name == "flowlayout_darkside" then
+                    table.remove(child.Children, k)
+                end
+            end
         end
     end
 
@@ -22,13 +27,50 @@ function Inspect:ShowElement(args)
     local senderM = getControlByName("Main", element)
     sender.Height = 200
     sender.Background = {50,50,50,255}
-    
-    local s = ""
-    for k, v in pairs(senderM) do
-        s = s .. k .. "=" .. v .. "\n"
-    end
-    sender.Text = s
 
+    local flow = CreateFlowLayout({
+        type = "flowlayout",
+        name = "flowlayout_darkside",
+        parent = sender.Name,
+        x = 2,
+        y = 2,
+        width = 761,
+        height = 195, -- set height to height of each child panel
+        background = "100,100,100,255",
+        border = "50,50,50,150",
+    })
+    
+    for k, v in pairs(senderM) do
+        if type(v) =="string" or type(v) == "number" or type(v) == "boolean" then
+            local identifier = math.random(5184, 98974)
+            local panel = CreatePanel({
+                type = "panel",
+                name = "panel" .. identifier,
+                parent = flow.Name,
+                x = 0,
+                y = 0,
+                width = 760,
+                height = 20, -- set height to height of each child panel
+                background = "1,1,1,1",
+                border = "50,50,50,150",
+                --mouseclick = "Inspect:ShowElement('" .. child.Name .. "," .. "panel" .. identifier .. "')"
+            })
+            panel.Children = {
+                [1] = CreateLabel({
+                    type = "label",
+                    name = "label" .. identifier,
+                    parent = "panel" .. identifier,
+                    x = 2,
+                    y = 2,
+                    color = "255,255,255,255",
+                    text = k .. " = " .. tostring(v),
+                })
+            }
+            flow.Children[#flow.Children+1] = panel
+        end
+    end
+
+    sender.Children[#sender.Children+1] = flow
 end
 
 function printChildren(node, indent, sender, comLabel)
