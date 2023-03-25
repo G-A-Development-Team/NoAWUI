40ef912e797c99be9447582f161af774487d0182
Carter Poe <gamechampcrafted@gmail.com>
2023-03-24 23:14:56 -0400

    Update music-kit-v2.0.lua

--- Examples/music-kit-v2.0.lua
+++ Examples/music-kit-v2.0.lua
@@ -93,6 +93,7 @@ end
 -- Grab the drop down box
 local plbChanger = getControlByName("Main", "plbChanger")
 
+
 -- Loop through all the kits
 for i = 1, table.getn(kits), 1 do
 	-- Add the kit to the plbChanger
